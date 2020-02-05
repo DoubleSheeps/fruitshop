14ef92185f7f0a0bae56a180a3a6ae837b0a7bd8
DoubleSheeps <568462483@qq.com>
2020-02-05 10:29:29 +0800

    数据库商品图片信息命名有误，已修正

--- src/main/java/com/fruitshop/springbootmybaties/service/impl/ItemServiceImpl.java
+++ src/main/java/com/fruitshop/springbootmybaties/service/impl/ItemServiceImpl.java
@@ -1,6 +1,8 @@
 package com.fruitshop.springbootmybaties.service.impl;
 
+import com.fruitshop.springbootmybaties.dao.ItemDOMapper;
 import com.fruitshop.springbootmybaties.dao.ItemStockDOMapper;
+import com.fruitshop.springbootmybaties.dataobject.ItemDO;
 import com.fruitshop.springbootmybaties.dataobject.ItemStockDO;
 import com.fruitshop.springbootmybaties.error.BusinessException;
 import com.fruitshop.springbootmybaties.error.EmBusinessError;
@@ -27,6 +29,9 @@ public class ItemServiceImpl implements ItemService {
             throw new BusinessException(EmBusinessError.ITEM_NOT_EXIST);
         }
         ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(id);
+        if(itemStockDO == null){
+            return null;
+        }
         ItemModel itemModel = this.convertFormDataObject(itemDo,itemStockDO);
         return itemModel;
     }
